b251af29bfac49835dc5c41e4588381c9576fbfa
Oscar Phillip Springsteen <pspringsteen160@gmail.com>
2020-09-04 04:20:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,18 @@
 def turn_count (board)
-  each
+  counter=0
+  board.each do |check|
+    if (check=="X" || check=="O")
+      counter+=1
+    end
+  end
+  return counter
 end
 
-def current_player
-  defi
+def current_player (counter)
+  check=counter%2
+  if (check==0)
+    return "X"
+  else
+    return "O"
+  end
 end
